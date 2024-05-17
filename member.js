6f6b9ecd9860d656e8ddb259a2d8e28be4665c01
Bastian <batro100@hhu.de>
2024-05-17 09:48:19 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var skills = {
+        name: "John",
+        age: 32,
+        skills: ["C#", "JavaScript", "Python"]
+    };
+    console.log(skills.name);
+    console.log(skills.age);
+    console.log(skills.skills);
+}
